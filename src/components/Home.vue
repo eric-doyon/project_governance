62be08b26cb0e4d0a7bbb4e9a8ebaccc06807758
eric-doyon <34319527+eric-doyon@users.noreply.github.com>
2018-06-13 13:50:23 -0400

    Create Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home-page">
+        <menu-section/>
+        <h2>Home</h2>
+    </div>
+</template>
+
+<script>
+import MenuSection from './menu/MenuSection.vue'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  props:{
+
+  },
+  components:{
+      'menu-section': MenuSection
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
