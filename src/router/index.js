f96c77e813b30ab67e90047e5cb1d25640698a01
eric-doyon <34319527+eric-doyon@users.noreply.github.com>
2018-06-13 13:48:45 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Organizations from '@/components/Organizations'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/org',
+        name: 'Organizations',
+        component: Organizations
+    },
   ]
 })
+
