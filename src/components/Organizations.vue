9dfcba9137f7b775368a7acdf9abc2d70828ed63
eric-doyon <34319527+eric-doyon@users.noreply.github.com>
2018-06-13 13:51:25 -0400

    Create Organizations.vue

---
+++ src/components/Organizations.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="org-page">
+        <menu-section/>
+        <h2>Organizations</h2>
+    </div>
+</template>
+
+<script>
+import MenuSection from './menu/MenuSection.vue'
+export default {
+  name: 'Organization',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  props:{
+
+  },
+  components:{
+      'menu-section': MenuSection
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
