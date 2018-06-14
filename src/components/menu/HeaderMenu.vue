98c45be7ad2f97eb42dc5363e9c856023f4acd92
eric-doyon <34319527+eric-doyon@users.noreply.github.com>
2018-06-13 20:44:06 -0400

    Add files via upload

---
+++ src/components/menu/HeaderMenu.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="menu-section">
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+        <router-link to="/org">Organizations</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  props:{
+
+  },
+  components:{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    @import '@/assets/css/main.scss';
+    .menu-section{
+        position: fixed;
+        top:0;
+        left:0;
+        width:100%;
+        height:$header-height;
+        background-color: $header-bg-color;
+    }
+</style>
---
+++ src/components/menu/NavMenu.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="nav-menu">
+        <header-menu/>
+        <ul>
+            <li>Home</li>
+            <li>Organizations</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  props:{
+
+  },
+  components:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+    @import '@/assets/css/main.scss';
+    .nav-menu{
+        position: fixed;
+        width: 200px;
+        height: 100%;
+        top: $header-height;
+        left:0;
+        background-color: $nav-menu-bg-color;
+    }
+</style>
