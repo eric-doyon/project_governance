7c3dc563159dcae0c4e2c82d8b3329bcd464a6dd
eric-doyon <34319527+eric-doyon@users.noreply.github.com>
2018-06-13 20:42:45 -0400

    Update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -47,6 +47,10 @@ module.exports = {
         options: vueLoaderConfig
       },
       {
+        test: /\.s[a|c]ss$/,
+        loader: 'sass-loader'
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
